2ae213ca5fe4c49345322775d7b537c868a1a10a
k1r0s <ciroreed@gmail.com>
2017-04-12 17:47:27 +0200

    added CustomParser from htmlparser2 (inherits)

--- tests/global.test.js
+++ tests/global.test.js
@@ -1,42 +1,23 @@
-var htmlparser = require("htmlparser2");
+var CustomParser = require("../lib/CustomParser");
 var assert = require("assert");
-
-var textResult = "";
-var currentTag = "";
-
-var parserOpts = {
-	onopentag: function(tagname, attrs){
-		if(currentTag){
-			textResult += "<" + currentTag.replace(":tag:", tagname) + ">";
-		}else{
-			textResult += "<" + tagname + ">";
-		}
-		currentTag = "";
-	},
-	onattribute: function(attr, val){
-		if(!currentTag) currentTag = ":tag:";
-		currentTag += " " + attr + "=\"" + val + "\"";
-	},
-	onclosetag: function(tagname){
-		textResult += "</" + tagname + ">";
-	},
-	ontext: function(t){
-		textResult += t;
-	},
-	onprocessinginstruction: function(text, d){
-		textResult += "<" + d + ">";
-	}
-};
 
 describe("transform current markup", function(){
     it("tests..", function(){
-		var test1 = '<span ::each="culo" sdasdasdasdsa><label><?= asd?></label></span>';
+		var test1 =
+        `
+            <span>
+                <p another="atr">This is an example</p>
+				<%=asd%>
 
-        var parser = new htmlparser.Parser(parserOpts);
+				some text
+            </span>
+        `;
+
+        var parser = new CustomParser();
 		parser.write(test1);
-		parser.end();
-		
-		assert(test1, textResult);
+		var result = parser.end();
+
+		assert(test1, result);
 
     });
 });
